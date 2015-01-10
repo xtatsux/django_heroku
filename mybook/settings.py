12f17b8b7e53ce39e07f260bca315284587d2358
tatsuya-takahashi <tatsu@Ragnarok.local>
2015-01-10 11:39:36 +0900

    modify

--- mybook/settings.py
+++ mybook/settings.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -63,6 +62,7 @@ WSGI_APPLICATION = 'mybook.wsgi.application'
 #     }
 # }
 
+import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
 # Internationalization
